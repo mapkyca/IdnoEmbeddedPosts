be785c90cc18c21619c057b9e7802f053da78e24
Marcus Povey <marcus@marcus-povey.co.uk>
2013-09-25 14:56:32 +0100

    First embed - needs to make it complete, and embed a full view.

--- EmbeddedPosts/templates/default/embeddedposts/embedcode.tpl.php
+++ EmbeddedPosts/templates/default/embeddedposts/embedcode.tpl.php
@@ -1,2 +1,11 @@
-<div class="idno-embed" data-url="<?= $vars['object']->getUUID(); ?>"><blockquote><?= $vars['object']->getDescription();?></blockquote></div>
+<div class="idno-embed" data-url="<?= $vars['object']->getUUID(); ?>">
+    <?= $vars['object']->draw();
+// TODO: Needs to include the activity stream activity view
+?>
+</div>
 <script src="<?= \Idno\Core\site()->config()->url . 'IdnoPlugins/EmbeddedPosts/js/' ?>embeddedposts.js"></script>
+<script type="text/javascript">
+    var style = document.createElement('style');
+    style.textContent = '@import "' + '<?= \Idno\Core\site()->config()->url; ?>IdnoPlugins/EmbeddedPosts/css/embeddedposts.css' + '"';
+    document.getElementsByTagName("body")[0].appendChild(style);
+</script>
