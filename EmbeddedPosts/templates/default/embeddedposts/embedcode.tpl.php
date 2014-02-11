12acf8053a30b398d6be1b7f075a702cb12fc8d2
Marcus Povey <marcus@marcus-povey.co.uk>
2014-02-11 14:33:00 +0000

    Improving embed code

--- EmbeddedPosts/templates/default/embeddedposts/embedcode.tpl.php
+++ EmbeddedPosts/templates/default/embeddedposts/embedcode.tpl.php
@@ -1,11 +1,22 @@
-<div class="idno-embed" data-url="<?= $vars['object']->getUUID(); ?>">
-    <?= $vars['object']->draw();
-// TODO: Needs to include the activity stream activity view
-?>
-</div>
-<script src="<?= \Idno\Core\site()->config()->url . 'IdnoPlugins/EmbeddedPosts/js/' ?>embeddedposts.js"></script>
+
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
 <script type="text/javascript">
     var style = document.createElement('style');
     style.textContent = '@import "' + '<?= \Idno\Core\site()->config()->url; ?>IdnoPlugins/EmbeddedPosts/css/embeddedposts.css' + '"';
     document.getElementsByTagName("body")[0].appendChild(style);
 </script>
+<script>
+            $(document).ready(function(){
+                $.ajax({
+                    url: '<?= $vars['object']->getUUID(); ?>?_t=jsonp',
+                    dataType: 'jsonp',
+                    success: function(jsondata){
+                        var entry_content = "<div class=\"e-content entry-conten\">" + jsondata.object.object.formattedContent + "</div>";
+                        
+                        
+                        $('#<?= $vars['object']->getSlug(); ?>').html(text);
+                    }
+                });
+            })
+        </script>
+        <div id="<?= $vars['object']->getSlug(); ?>"></div>
