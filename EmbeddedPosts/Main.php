fadb5b98b78528619cdbe8d0b536e7463497c762
Marcus Povey <marcus@marcus-povey.co.uk>
2014-02-14 19:27:25 +0000

    Only show embed links outside of embed view

--- EmbeddedPosts/Main.php
+++ EmbeddedPosts/Main.php
@@ -3,9 +3,11 @@
     namespace IdnoPlugins\EmbeddedPosts {
         class Main extends \Idno\Common\Plugin {
             function registerPages() {
-                // Share
-                \Idno\Core\site()->template()->extendTemplate('content/end','embeddedposts/share');
-                \Idno\Core\site()->template()->extendTemplate('content/end','embeddedposts/entity');
+                // Share, but only if this isn't in the embed view
+                if ($_REQUEST['_t'] != 'embed') { // TODO: Do this properly!
+                    \Idno\Core\site()->template()->extendTemplate('content/end','embeddedposts/share');
+                    \Idno\Core\site()->template()->extendTemplate('content/end','embeddedposts/entity');
+                }
             }
         }
     }
