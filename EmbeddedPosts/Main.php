a324a2adc0e9c9ce06723b7ca4fd7fb05ca9c448
Marcus Povey <marcus@marcus-povey.co.uk>
2014-03-07 15:52:33 +0000

    Discovery links

--- EmbeddedPosts/Main.php
+++ EmbeddedPosts/Main.php
@@ -3,6 +3,9 @@
     namespace IdnoPlugins\EmbeddedPosts {
         class Main extends \Idno\Common\Plugin {
             function registerPages() {
+                // Add oembed descovery
+                 \Idno\Core\site()->template()->extendTemplate('shell/head','embeddedposts/header');
+                
                 // Share, but only if this isn't in the embed view
                 if ($_REQUEST['_t'] != 'embed') { // TODO: Do this properly!
                     \Idno\Core\site()->template()->extendTemplate('content/end/links','embeddedposts/share');
---
+++ EmbeddedPosts/templates/default/embeddedposts/header.tpl.php
@@ -0,0 +1,10 @@
+<?php
+
+if (\Idno\Core\site()->currentPage()->isPermalink()) {
+  ?>
+<link rel="alternate" type="application/json+oembed"
+      href="<?= \Idno\Core\site()->config()->url; ?>oembed/?url=<?= urlencode($vars['object']->getUrl());?>&format=json"
+  title="<?= $vars['object']->getTitle(); ?>" />
+<?php
+}
+?>
