6a73c61edd8fb56471e6cd013b822825512017f3
Marcus Povey <marcus@marcus-povey.co.uk>
2014-04-06 12:25:48 +0100

    OEmbed page handler endpoint

--- EmbeddedPosts/Main.php
+++ EmbeddedPosts/Main.php
@@ -5,6 +5,10 @@
             function registerPages() {
                 // Add oembed descovery
                  \Idno\Core\site()->template()->extendTemplate('shell/head','embeddedposts/header');
+		 
+		 
+		// Register handler page
+		 \Idno\Core\site()->addPageHandler('/oembed', '\IdnoPlugins\EmbeddedPosts\Pages\OEmbed');
                 
                 // Share, but only if this isn't in the embed view
                 if ($_REQUEST['_t'] != 'embed') { // TODO: Do this properly!
