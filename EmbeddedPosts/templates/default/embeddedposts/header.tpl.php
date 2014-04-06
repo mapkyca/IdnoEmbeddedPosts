74355efde0d3d9504655b480982957597e310482
Marcus Povey <marcus@marcus-povey.co.uk>
2014-04-06 12:25:27 +0100

    Descovery header

--- EmbeddedPosts/templates/default/embeddedposts/header.tpl.php
+++ EmbeddedPosts/templates/default/embeddedposts/header.tpl.php
@@ -4,7 +4,7 @@ if (\Idno\Core\site()->currentPage()->isPermalink()) {
   ?>
 <link rel="alternate" type="application/json+oembed"
       href="<?= \Idno\Core\site()->config()->url; ?>oembed/?url=<?= urlencode($vars['object']->getUrl());?>&format=json"
-  title="<?= $vars['object']->getTitle(); ?>" />
+      title="<?= $vars['object']->getTitle(); ?>" />
 <?php
 }
 ?>
