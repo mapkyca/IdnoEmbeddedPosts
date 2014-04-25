d3ee8dc39a81cd1edd784f10419a919b2eaec309
Marcus Povey <marcus@marcus-povey.co.uk>
2014-04-25 11:24:44 +0100

    Fixing notice errors

--- EmbeddedPosts/Main.php
+++ EmbeddedPosts/Main.php
@@ -11,7 +11,7 @@
 		 \Idno\Core\site()->addPageHandler('/oembed', '\IdnoPlugins\EmbeddedPosts\Pages\OEmbed');
                 
                 // Share, but only if this isn't in the embed view
-                if ($_REQUEST['_t'] != 'embed') { // TODO: Do this properly!
+                if ((!isset($_REQUEST['_t'])) || ($_REQUEST['_t'] != 'embed')) { // TODO: Do this properly!
                     \Idno\Core\site()->template()->extendTemplate('content/end/links','embeddedposts/share');
                     \Idno\Core\site()->template()->extendTemplate('content/end','embeddedposts/entity');
                 }
