32371061b5c9a0ec6a3b2e569e53299e6eccf0fa
Marcus Povey <marcus@marcus-povey.co.uk>
2014-10-10 09:11:52 +0100

    Check that object exists

--- EmbeddedPosts/templates/default/embeddedposts/header.tpl.php
+++ EmbeddedPosts/templates/default/embeddedposts/header.tpl.php
@@ -1,6 +1,6 @@
 <?php
 
-if (\Idno\Core\site()->currentPage() && \Idno\Core\site()->currentPage()->isPermalink()) {
+if (\Idno\Core\site()->currentPage() && \Idno\Core\site()->currentPage()->isPermalink() && $vars['object']) {
   ?>
 <link rel="alternate" type="application/json+oembed"
       href="<?= \Idno\Core\site()->config()->url; ?>oembed/?url=<?= urlencode($vars['object']->getUrl());?>&format=json"
