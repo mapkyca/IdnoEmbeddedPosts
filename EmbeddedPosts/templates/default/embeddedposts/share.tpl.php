edeb231a4252f9fa1a8df66888cf0a5f756aaca2
Marcus Povey <marcus@marcus-povey.co.uk>
2015-04-16 11:32:12 +0100

    Not sure what I changed, but it seems to work

--- EmbeddedPosts/templates/default/embeddedposts/share.tpl.php
+++ EmbeddedPosts/templates/default/embeddedposts/share.tpl.php
@@ -1,1 +1,1 @@
-<a href="#idno-embedcode-<?php echo $vars['object']->getID(); ?>" class="idno-share-button" data-toggle="modal" data-share="<?php echo $vars['object']->getID(); ?>"><i class="icon-export-alt"> </i></a>
+<a href="#idno-embedcode-<?php echo $vars['object']->getID(); ?>" class="idno-share" data-toggle="modal" data-share="<?php echo $vars['object']->getID(); ?>"><i class="icon-export-alt"> </i></a>
