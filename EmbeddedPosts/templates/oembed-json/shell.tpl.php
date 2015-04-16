58869faa905364ac651b554d925def784f9a0829
Marcus Povey <marcus@marcus-povey.co.uk>
2015-04-16 12:43:58 +0100

    Working oembed

--- EmbeddedPosts/templates/oembed-json/shell.tpl.php
+++ EmbeddedPosts/templates/oembed-json/shell.tpl.php
@@ -1,15 +1,17 @@
 <?php
 
     header('Content-type: application/json');
-
+    
     if (!($callback = \Idno\Core\site()->currentPage()->getInput('callback'))) {
         if (!($callback = \Idno\Core\site()->currentPage()->getInput('jsonp'))) {
             if (!($callback = \Idno\Core\site()->currentPage()->getInput('response'))) {
-		echo $vars['body'];
+		$body = $vars['body'];
 	    }
         }
     }
  else {
-     echo "$callback({$vars['body']})";
+     $body = "$callback({$vars['body']})";
+     
  }
     
+ echo $body;
