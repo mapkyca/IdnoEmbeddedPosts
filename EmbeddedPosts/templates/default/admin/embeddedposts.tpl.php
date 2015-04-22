2bd987741a4f0ff8239e32d8a251fa0ba040ae6e
Marcus Povey <marcus@marcus-povey.co.uk>
2015-04-22 14:11:06 +0100

    Embedded post whitelist config

---
+++ EmbeddedPosts/templates/default/admin/embeddedposts.tpl.php
@@ -0,0 +1,35 @@
+<div class="row">
+
+    <div class="span10 offset1">
+        <h1>Enbedded Posts</h1>
+        <?=$this->draw('admin/menu')?>
+    </div>
+
+</div>
+<div class="row">
+    <div class="span10 offset1">
+        <form action="/admin/embeddedposts/" class="form-horizontal" method="post">
+            <div class="control-group">
+                <div class="controls">
+                    <p>Configure a whitelist of other Known domains for which embedded posts will be automatically activated, comma, semicolon or new line separated.</p>
+                </div>
+            </div>
+            <div class="control-group">
+		<label class="control-label" for="whitelist">Domain Whitelist</label>
+		<div class="controls">
+                <textarea name="whitelist"><?php 
+		foreach (\Idno\Core\site()->config()->embeddedposts['whitelist'] as $line) {
+		    echo htmlspecialchars($line)."\n";
+		}
+		?></textarea>
+		</div>
+            </div>
+            <div class="control-group">
+                <div class="controls">
+                    <button type="submit" class="btn btn-primary">Save</button>
+                </div>
+            </div>
+            <?= \Idno\Core\site()->actions()->signForm('/admin/embeddedposts/')?>
+        </form>
+    </div>
+</div>
