78ac2e4358ac54977d68061d4c991cd4df5aed83
Marcus Povey <marcus@marcus-povey.co.uk>
2015-04-22 14:17:59 +0100

    Saving whitelist

---
+++ EmbeddedPosts/Pages/Admin.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * EmbeddedPosts pages
+ */
+
+namespace IdnoPlugins\EmbeddedPosts\Pages {
+
+    /**
+     * Default class to serve EmbeddedPosts settings in administration
+     */
+    class Admin extends \Idno\Common\Page {
+
+	function getContent() {
+	    $this->adminGatekeeper(); // Admins only
+	    $t = \Idno\Core\site()->template();
+	    $body = $t->draw('admin/embeddedposts');
+	    $t->__(['title' => 'Embedded Posts', 'body' => $body])->drawPage();
+	}
+
+	function postContent() {
+	    $this->adminGatekeeper(); // Admins only
+
+	    $whitelist = $this->getInput('whitelist');
+
+	    if ($whitelist) {
+
+		$array = preg_split('/[\s,;:]+/', $whitelist);
+		
+		\Idno\Core\site()->config->config['embeddedposts'] = [
+		    'whitelist' => $array
+		];
+		
+		
+	    \Idno\Core\site()->config()->save();
+	    \Idno\Core\site()->session()->addMessage('Your Embedded posts settings were saved.');
+	    }
+
+	    $this->forward('/admin/embeddedposts/');
+	}
+
+    }
+
+}
