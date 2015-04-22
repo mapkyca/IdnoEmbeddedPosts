83f9bcfe31379acf48c7f37133ed749436e2a581
Marcus Povey <marcus@marcus-povey.co.uk>
2015-04-22 14:36:31 +0100

    Handling whitelists http

--- EmbeddedPosts/templates/default/embeddedposts/linking/embed.tpl.php
+++ EmbeddedPosts/templates/default/embeddedposts/linking/embed.tpl.php
@@ -13,7 +13,7 @@ foreach ($whitelist as $knownknowns) {
     
     if ($knownknowns) {
 	// Correct slashes
-	if ((strpos($knownknowns, 'http://') == false) && (strpos($knownknowns, 'https://') == false)) {
+	if ((strpos($knownknowns, 'http://') === false) && (strpos($knownknowns, 'https://') === false)) {
 	    $knownknowns = 'http://' . $knownknowns;
 	}
 	$knownknowns = str_replace('/', '\\/', $knownknowns);
