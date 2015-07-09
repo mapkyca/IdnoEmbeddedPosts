5a3ad3f2c47288c7693c14713fbfcacf6e1ac790
Marcus Povey <marcus@marcus-povey.co.uk>
2015-07-09 11:16:39 +0100

    Bootstrap 3

--- EmbeddedPosts/templates/default/admin/embeddedposts.tpl.php
+++ EmbeddedPosts/templates/default/admin/embeddedposts.tpl.php
@@ -1,13 +1,13 @@
 <div class="row">
 
-    <div class="span10 offset1">
+    <div class="col-md-10 col-md-offset-1">
         <h1>Enbedded Posts</h1>
         <?=$this->draw('admin/menu')?>
     </div>
 
 </div>
 <div class="row">
-    <div class="span10 offset1">
+    <div class="col-md-10 col-md-offset-1">
         <form action="/admin/embeddedposts/" class="form-horizontal" method="post">
             <div class="control-group">
                 <div class="controls">
