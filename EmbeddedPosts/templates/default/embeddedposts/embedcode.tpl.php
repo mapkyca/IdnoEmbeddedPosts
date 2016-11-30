000ae37bf969618f70ec288edfe63099eee8cbe2
Marcus Povey <marcus@marcus-povey.co.uk>
2016-11-30 15:17:10 +0000

    Handle null object

--- EmbeddedPosts/templates/default/embeddedposts/embedcode.tpl.php
+++ EmbeddedPosts/templates/default/embeddedposts/embedcode.tpl.php
@@ -1,16 +1,23 @@
 <?php
-$noschema = $vars['object']->getUUID(); //substr(\Idno\Core\site()->config()->url, 0);
-$noschema = str_replace('http:', '', $noschema);
-$noschema = str_replace('https:', '', $noschema);
 
-$width = null;
-if (!empty($vars['width']))
-	$width = $vars['width'];
-$height = null;
-if (!empty($vars['height']))
-    $height = $vars['height']; 
+if (!empty($vars['object'])) 
+{
+    $noschema = $vars['object']->getUUID(); //substr(\Idno\Core\site()->config()->url, 0);
+    $noschema = str_replace('http:', '', $noschema);
+    $noschema = str_replace('https:', '', $noschema);
 
-if (!$width) $width = 500;
-if (!$height) $height = 200;
+    $width = null;
+    if (!empty($vars['width']))
+	    $width = $vars['width'];
+    $height = null;
+    if (!empty($vars['height']))
+	$height = $vars['height']; 
 
-?><iframe src="<?= $noschema; ?>?_t=embed" seamless style="border: 0px; overflow: hidden; width: <?= $width; ?>px; height: <?= $height; ?>px;"></iframe>
+    if (!$width) $width = 500;
+    if (!$height) $height = 200;
+
+    ?>
+<iframe src="<?= $noschema; ?>?_t=embed" seamless style="border: 0px; overflow: hidden; width: <?= $width; ?>px; height: <?= $height; ?>px;"></iframe>
+<?php 
+} 
+?>
